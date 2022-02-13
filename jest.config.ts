612ec7d701202721c73bfb306e0eafdc393fabdc
ecyrbe <ecyrbe@gmail.com>
2022-02-13 22:12:16 +0100

    test: add jest and impl tests

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "@jest/types";
+
+// Objet synchrone
+const config: Config.InitialOptions = {
+  verbose: true,
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  rootDir: "./",
+  testRegex: ".(spec|test).tsx?$",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  coverageDirectory: "./coverage",
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85,
+    },
+  },
+  testEnvironment: "node",
+};
+export default config;
