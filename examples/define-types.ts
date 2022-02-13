e2cb60954bfc14e0628ef50ba37a2b0b5c5f8310
ecyrbe <ecyrbe@gmail.com>
2022-02-13 19:57:55 +0100

    chore: add example with predefined schemas and types

---
+++ examples/define-types.ts
@@ -0,0 +1,66 @@
+import { Zodios } from "../src/index";
+import { z } from "zod";
+
+// you can define schema before declaring the API to get back the type
+const userSchema = z
+  .object({
+    id: z.number(),
+    name: z.string(),
+  })
+  .required();
+
+const usersSchema = z.array(userSchema);
+
+// you can then get back the types
+type User = z.infer<typeof userSchema>;
+type Users = z.infer<typeof usersSchema>;
+
+// and then use them in your API
+async function bootstrap() {
+  const apiClient = new Zodios(
+    "https://jsonplaceholder.typicode.com",
+    {
+      getToken: () => Promise.resolve("token"),
+    },
+    [
+      {
+        method: "get",
+        path: "/users",
+        description: "Get all users",
+        parameters: [
+          {
+            name: "q",
+            type: "Query",
+            schema: z.string(),
+          },
+          {
+            name: "page",
+            type: "Query",
+            schema: z.string().optional(),
+          },
+        ],
+        response: usersSchema,
+      },
+      {
+        method: "get",
+        path: "/users/:id",
+        description: "Get a user",
+        parameters: [
+          {
+            type: "Path",
+            name: "id",
+            schema: z.number(),
+          },
+        ],
+        response: userSchema,
+      },
+    ] as const
+  );
+
+  const users = await apiClient.get("/users", { queries: { q: "Nicholas" } });
+  console.log(users);
+  const user = await apiClient.get("/users/:id", { params: { id: 7 } });
+  console.log(user);
+}
+
+bootstrap();
