884b295d3ded6c674d857a5aa5315ef4d83ab704
ecyrbe <ecyrbe@gmail.com>
2022-02-19 13:33:25 +0100

    feat(react): only use zodios params as keys for use query

--- src/react/hooks.ts
+++ src/react/hooks.ts
@@ -2,7 +2,12 @@ import { useContext } from "react";
 import { useQuery, UseQueryOptions } from "react-query";
 import { ZodiosContext } from "./provider";
 import { Zodios, ZodiosEnpointDescriptions } from "../index";
-import { Paths, ZodiosRequestOptions } from "../zodios.types";
+import {
+  AnyZodiosRequestOptions,
+  Paths,
+  ZodiosRequestOptions,
+} from "../zodios.types";
+import { pick } from "../utils";
 
 /**
  * React hook to call an api "get" endpoint.
@@ -11,17 +16,18 @@ import { Paths, ZodiosRequestOptions } from "../zodios.types";
  * else you will not have nice autocompletions in your IDE.
  * It should be used in combination with the ZodiosProvider
  * @param apiName - the name of the api used in Zodios Provider
- * @param url - the url to call
+ * @param path - the path to api endpoint
  * @param config - the config to setup axios options and parameters
  * @param queryOptions - the query options to setup react-query options
  * @returns the response validated with zod
  *
  * @example ```typescript
+ *   type Api = ApiOf<typeof myZodiosClient>;
  *   function useJsonPlaceholder<Path extends Paths<Api, "get">>(
- *     url: Path,
+ *     path: Path,
  *     config?: ZodiosRequestOptions<Api, "get", Path>
  *   ) {
- *     return useZodios("https://jsonplaceholder.typicode.com", url, config);
+ *     return useZodios("https://jsonplaceholder.typicode.com", path, config);
  *   }
  * ```
  */
@@ -31,18 +37,22 @@ export function useZodios<
   Path extends Paths<Api, "get">
 >(
   baseUrl: URL,
-  url: Path,
+  path: Path,
   config?: ZodiosRequestOptions<Api, "get", Path>,
   queryOptions?: Omit<UseQueryOptions, "queryKey" | "queryFn">
 ) {
   const zodios = useContext(ZodiosContext)[baseUrl] as Zodios<URL, Api>;
-  const query = () => zodios.get(url, config);
-  return useQuery(
-    [baseUrl, url, config],
-    query,
-    queryOptions as Omit<
-      UseQueryOptions<Awaited<ReturnType<typeof query>>>,
-      "queryKey" | "queryFn"
-    >
-  );
+  const params = pick(config as AnyZodiosRequestOptions | undefined, [
+    "params",
+    "queries",
+  ]);
+  const keys = [baseUrl, path, params];
+  const query = () => zodios.get(path, config);
+  type QueryOptions =
+    | Omit<
+        UseQueryOptions<Awaited<ReturnType<typeof query>>>,
+        "queryKey" | "queryFn"
+      >
+    | undefined;
+  return useQuery(keys, query, queryOptions as QueryOptions);
 }
