027b4dcae1f896c73e613733fd500f27875861cc
ecyrbe <ecyrbe@gmail.com>
2022-05-03 21:30:29 +0200

    chore: update coverage

--- jest.config.ts
+++ jest.config.ts
@@ -12,7 +12,7 @@ const config: Config.InitialOptions = {
   coverageDirectory: "./coverage",
   coverageThreshold: {
     global: {
-      branches: 85,
+      branches: 80,
       functions: 85,
       lines: 85,
       statements: 85,
