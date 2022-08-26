c538f3686bf4b65549b23296c1330bad1bf94bc7
ecyrbe <ecyrbe@gmail.com>
2022-08-27 00:59:39 +0200

    fix: website url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Zodios",
   tagline: "End-to-end typesafe REST API toolbox",
-  url: "https://zodios.org",
+  url: "https://www.zodios.org",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
