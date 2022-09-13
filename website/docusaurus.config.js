c4e563d9e532f6dd78124f9a10c09d2452a702c5
ecyrbe <ecyrbe@gmail.com>
2022-09-14 01:44:06 +0200

    feat: add analytics

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -45,6 +45,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        googleAnalytics: {
+          trackingID: "G-J0FDBSMYKD",
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
