e634dc3989b2d998ad856e4934b06deb0b06d964
ecyrbe <ecyrbe@gmail.com>
2022-10-28 21:01:57 +0200

    docs: add social img

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -56,6 +56,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      image: "/img/zodios-social.png",
       navbar: {
         title: "Zodios",
         logo: {
