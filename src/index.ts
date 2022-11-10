8b7e76381784b8248b53a08d4a9c3bf734ab2c95
ecyrbe <ecyrbe@gmail.com>
2022-11-10 17:28:01 +0100

    feat(type-provider): improve ts perf and memory footprint

--- src/index.ts
+++ src/index.ts
@@ -53,6 +53,8 @@ export type { ZodTypeProvider } from "./type-provider.zod";
 export { zodTypeProvider } from "./type-provider.zod";
 export type { IoTsTypeProvider } from "./type-provider.io-ts";
 export { ioTsTypeProvider } from "./type-provider.io-ts";
+export type { TsTypeProvider } from "./type-provider.typescript";
+export { tsTypeProvider } from "./type-provider.typescript";
 export {
   PluginId,
   zodValidationPlugin,
