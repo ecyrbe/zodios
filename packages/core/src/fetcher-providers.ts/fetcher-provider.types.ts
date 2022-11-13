9cfb726703c20f4f5acc311d02a16cec5e4300e9
ecyrbe <ecyrbe@gmail.com>
2022-11-13 15:18:45 +0100

    feat(fetcher-provider): first impl of axios fetcher provider

---
+++ packages/core/src/fetcher-providers.ts/fetcher-provider.types.ts
@@ -0,0 +1,42 @@
+/**
+ * A type provider for Fetcher
+ * allows to define request, response and error types for a fetcher
+ */
+export interface AnyZodiosFetcherProvider {
+  /**
+   * inputs types to call fetcher
+   */
+  arg1: unknown;
+  arg2: unknown;
+
+  /**
+   * config types to call fetcher
+   */
+  config: unknown;
+  response: unknown;
+  error: unknown;
+}
+
+export type TypeOfFetcherConfig<
+  FetcherProvider extends AnyZodiosFetcherProvider
+> = FetcherProvider["config"];
+
+export type TypeOfFetcherError<
+  FetcherProvider extends AnyZodiosFetcherProvider,
+  Response,
+  Status
+> = (FetcherProvider & {
+  arg1: Response;
+  arg2: Status;
+})["error"];
+
+export type TypeOfFetcherResponse<
+  FetcherProvider extends AnyZodiosFetcherProvider
+> = FetcherProvider["response"];
+
+export type ZodiosRuntimeFetcherProvider<
+  FetcherProvider extends AnyZodiosFetcherProvider
+> = {
+  readonly _provider?: FetcherProvider;
+  fetch: (params: any) => Promise<any>;
+};
