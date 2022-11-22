ecdaf92a62dbc0249c529e9f971dd7d32c037e40
ecyrbe <ecyrbe@gmail.com>
2022-11-22 01:00:52 +0100

    docs(changeset): add basic mocks helpers

--- packages/axios/src/axios-provider.ts
+++ packages/axios/src/axios-provider.ts
@@ -42,7 +42,7 @@ export const axiosProvider: ZodiosRuntimeFetcherProvider<AxiosProvider> & {
   instance: AxiosInstance;
 } = {
   instance: undefined as any,
-  create(
+  init(
     options: {
       baseURL?: string;
     } & AxiosProviderOptions
