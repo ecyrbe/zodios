debe088bd1d0268a2a6bdc17923613d7df7d0b87
ecyrbe <ecyrbe@gmail.com>
2022-11-24 23:46:54 +0100

    docs(changeset): fix(testing): safely export hooks

--- packages/core/src/zodios.test.ts
+++ packages/core/src/zodios.test.ts
@@ -31,12 +31,129 @@ globalThis.FormData = class FormData {
   }
 };
 
-import { ZodiosCore } from "./zodios";
-import { ZodiosError } from "./zodios-error";
-import { ZodiosPlugin } from "./zodios.types";
-import { apiBuilder } from "./api";
-import { AnyZodiosFetcherProvider } from "./fetcher-providers";
-import { zodiosMocks } from "../../testing/src/mock-provider";
+import {
+  ZodiosCore,
+  ZodiosError,
+  apiBuilder,
+  setFetcherHook,
+  clearFetcherHook,
+} from "./index";
+
+import type {
+  AnyZodiosFetcherProvider,
+  AnyZodiosRequestOptions,
+  ZodiosPlugin,
+  ZodiosRuntimeFetcherProvider,
+} from "./index";
+
+interface MockProvider extends AnyZodiosFetcherProvider {
+  options: {};
+  config: {
+    baseURL?: string;
+    body?: unknown;
+    auth?: { username: string; password: string };
+    validateStatus?: (status: number) => boolean;
+    timeout?: number;
+    responseType?: "arraybuffer" | "blob" | "json" | "text" | "stream";
+  };
+  response: unknown;
+  error: unknown;
+}
+
+const mockProvider: ZodiosRuntimeFetcherProvider<MockProvider> = {
+  init() {},
+  async fetch(config: AnyZodiosRequestOptions<MockProvider>) {
+    const requestConfig = {
+      ...config,
+      baseURL: this.baseURL,
+    };
+    for (const [{ method, url }, callback] of registeredMocks) {
+      if (method === requestConfig.method && url === requestConfig.url) {
+        const result = (await callback(
+          requestConfig
+        )) as Required<MockResponse>;
+        if (
+          (config.validateStatus && !config.validateStatus(result.status)) ||
+          result.status > 299
+        ) {
+          const err = new Error(
+            `Request failed with status code ${result.status}`
+          ) as Error & { code: string; config: any; response: any };
+          err.code = `ERR_STATUS_${result.status}`;
+          err.config = requestConfig;
+          err.response = result;
+          throw err;
+        }
+        return result;
+      }
+    }
+    throw new Error(`No mocks found for ${requestConfig.url}`);
+  },
+};
+
+type MockResponse<Data = unknown> = {
+  readonly headers?: Record<string, string>;
+  readonly status?: number;
+  readonly statusText?: string;
+  readonly data?: Data;
+};
+
+interface ZodiosMockBase {
+  install(): void;
+  uninstall(): void;
+  reset(): void;
+  mockRequest(
+    method: string,
+    path: string,
+    callback: (
+      config: AnyZodiosRequestOptions<MockProvider>
+    ) => Promise<MockResponse>
+  ): void;
+  mockResponse(method: string, path: string, response: MockResponse): void;
+}
+
+const registeredMocks = new Map<
+  { method: string; url: string },
+  (config: AnyZodiosRequestOptions<MockProvider>) => Promise<MockResponse>
+>();
+
+const zodiosMocks: ZodiosMockBase = {
+  install() {
+    setFetcherHook(mockProvider);
+  },
+  uninstall() {
+    registeredMocks.clear();
+    clearFetcherHook();
+  },
+  reset() {
+    registeredMocks.clear();
+  },
+  mockRequest(
+    method: string,
+    path: string,
+    callback: (
+      config: AnyZodiosRequestOptions<MockProvider>
+    ) => Promise<MockResponse>
+  ) {
+    registeredMocks.set({ method, url: path }, callback);
+  },
+  mockResponse(method: string, path: string, response: MockResponse) {
+    registeredMocks.set(
+      {
+        method,
+        url: path,
+      },
+      () =>
+        Promise.resolve({
+          headers: {},
+          data: {},
+          status: 200,
+          statusText: "OK",
+          ...response,
+        })
+    );
+  },
+};
 
 describe("Zodios", () => {
   beforeAll(async () => {
