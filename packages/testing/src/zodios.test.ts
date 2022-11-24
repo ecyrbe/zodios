4f3f15733d9c2fde37c0018c933105ed1396b691
ecyrbe <ecyrbe@gmail.com>
2022-11-24 18:58:11 +0100

    docs(changeset): feat(testing): add response testing handlers

--- packages/testing/src/zodios.test.ts
+++ packages/testing/src/zodios.test.ts
@@ -82,10 +82,187 @@ const zodios = new ZodiosCore(api, { fetcherProvider: mockProvider });
 const mocks = new ZodiosMocks(zodios);
 
 describe("Zodios", () => {
-  beforeAll(async () => {
-    ZodiosMocks.install();
-    mocks.get("/users", async (config) => {
-      return {
+  describe("dynamic mocks", () => {
+    beforeAll(() => {
+      ZodiosMocks.install();
+      mocks.get("/users", async (config) => {
+        return {
+          data: [
+            {
+              id: 1,
+              name: "John Doe",
+              email: "john.doe@test.com",
+            },
+          ],
+        };
+      });
+      mocks.get("/users/:id", async (config) => {
+        if (config.params.id === 1) {
+          return {
+            data: {
+              id: 1,
+              name: "John Doe",
+              email: "john.doe@test.com",
+            },
+          };
+        }
+        return {
+          status: 404,
+          data: {
+            message: "User not found",
+          },
+        };
+      });
+      mocks.post("/users", async (config) => {
+        return {
+          data: {
+            id: 1,
+            ...config.body,
+          },
+        };
+      });
+      mocks.put("/users/:id", async (config) => {
+        if (config.params.id === 1) {
+          return {
+            data: {
+              ...config.body,
+            },
+          };
+        }
+        return {
+          status: 404,
+          data: {
+            message: "User not found",
+          },
+        };
+      });
+      mocks.patch("/users/:id", async (config) => {
+        if (config.params.id === 1) {
+          return {
+            data: {
+              ...config.body,
+            },
+          };
+        }
+        return {
+          status: 404,
+          data: {
+            message: "User not found",
+          },
+        };
+      });
+      mocks.delete("/users/:id", async (config) => {
+        if (config.params.id === 1) {
+          return {
+            data: {
+              id: 1,
+              name: "John Doe",
+              email: "john.doe@test.com",
+            },
+          };
+        }
+        return {
+          status: 404,
+          data: {
+            message: "User not found",
+          },
+        };
+      });
+    });
+
+    afterAll(() => {
+      ZodiosMocks.uninstall();
+    });
+
+    it("should be defined", () => {
+      expect(ZodiosMocks).toBeDefined();
+    });
+
+    it("should be able to mock a request", async () => {
+      const response = await zodios.get("/users");
+      expect(response).toEqual([
+        {
+          id: 1,
+          name: "John Doe",
+          email: "john.doe@test.com",
+        },
+      ]);
+    });
+    it("should be able to mock a request with params", async () => {
+      const response = await zodios.get("/users/:id", { params: { id: 1 } });
+      expect(response).toEqual({
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@test.com",
+      });
+    });
+    it("should be able to mock errors of a request with params", async () => {
+      let error;
+      try {
+        const response = await zodios.get("/users/:id", { params: { id: 2 } });
+      } catch (e) {
+        error = e;
+      }
+      expect(error).toBeDefined();
+      expect((error as any).response.status).toEqual(404);
+    });
+    it("should be able to mock a post request", async () => {
+      const response = await zodios.post("/users", {
+        body: { name: "John Doe", email: "john.doe@test-post.com" },
+      });
+      expect(response).toEqual({
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@test-post.com",
+      });
+    });
+    it("should be able to mock a put request", async () => {
+      const response = await zodios.put("/users/:id", {
+        params: { id: 1 },
+        body: { id: 1, name: "John Doe", email: "john.doe@test-put.com" },
+      });
+      expect(response).toEqual({
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@test-put.com",
+      });
+    });
+    it("should be able to mock a patch request", async () => {
+      const response = await zodios.patch("/users/:id", {
+        params: { id: 1 },
+        body: { id: 1, name: "John Doe", email: "john.doe@test-patch.com" },
+      });
+      expect(response).toEqual({
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@test-patch.com",
+      });
+    });
+    it("should be able to mock a delete request", async () => {
+      const response = await zodios.delete("/users/:id", {
+        params: { id: 1 },
+      });
+      expect(response).toEqual({
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@test.com",
+      });
+    });
+  });
+
+  describe("static mocks", () => {
+    beforeAll(() => {
+      ZodiosMocks.install();
+    });
+    afterAll(() => {
+      ZodiosMocks.uninstall();
+    });
+    beforeEach(() => {
+      ZodiosMocks.reset();
+    });
+
+    it("should be able to mock a request", async () => {
+      mocks.getResponse("/users", {
         data: [
           {
             id: 1,
@@ -93,158 +270,120 @@ describe("Zodios", () => {
             email: "john.doe@test.com",
           },
         ],
-      };
-    });
-    mocks.get("/users/:id", async (config) => {
-      if (config.params.id === 1) {
-        return {
-          data: {
-            id: 1,
-            name: "John Doe",
-            email: "john.doe@test.com",
-          },
-        };
-      }
-      return {
-        status: 404,
-        data: {
-          message: "User not found",
+      });
+      const response = await zodios.get("/users");
+      expect(response).toEqual([
+        {
+          id: 1,
+          name: "John Doe",
+          email: "john.doe@test.com",
         },
-      };
+      ]);
     });
-    mocks.post("/users", async (config) => {
-      return {
+    it("should be able to mock a request with params", async () => {
+      mocks.getResponse("/users/:id", {
         data: {
           id: 1,
-          ...config.body,
+          name: "John Doe",
+          email: "john.doe@test.com",
         },
-      };
-    });
-    mocks.put("/users/:id", async (config) => {
-      if (config.params.id === 1) {
-        return {
-          data: {
-            ...config.body,
-          },
-        };
-      }
-      return {
-        status: 404,
-        data: {
-          message: "User not found",
-        },
-      };
-    });
-    mocks.patch("/users/:id", async (config) => {
-      if (config.params.id === 1) {
-        return {
-          data: {
-            ...config.body,
-          },
-        };
-      }
-      return {
-        status: 404,
-        data: {
-          message: "User not found",
-        },
-      };
-    });
-    mocks.delete("/users/:id", async (config) => {
-      if (config.params.id === 1) {
-        return {
-          data: {
-            id: 1,
-            name: "John Doe",
-            email: "john.doe@test.com",
-          },
-        };
-      }
-      return {
-        status: 404,
-        data: {
-          message: "User not found",
-        },
-      };
-    });
-  });
+      });
 
-  afterAll(() => {
-    ZodiosMocks.uninstall();
-  });
-
-  it("should be defined", () => {
-    expect(ZodiosMocks).toBeDefined();
-  });
-
-  it("should be able to mock a request", async () => {
-    const response = await zodios.get("/users");
-    expect(response).toEqual([
-      {
+      const response = await zodios.get("/users/:id", { params: { id: 1 } });
+      expect(response).toEqual({
         id: 1,
         name: "John Doe",
         email: "john.doe@test.com",
-      },
-    ]);
-  });
-  it("should be able to mock a request with params", async () => {
-    const response = await zodios.get("/users/:id", { params: { id: 1 } });
-    expect(response).toEqual({
-      id: 1,
-      name: "John Doe",
-      email: "john.doe@test.com",
+      });
     });
-  });
-  it("should be able to mock errors of a request with params", async () => {
-    let error;
-    try {
-      const response = await zodios.get("/users/:id", { params: { id: 2 } });
-    } catch (e) {
-      error = e;
-    }
-    expect(error).toBeDefined();
-    expect((error as any).response.status).toEqual(404);
-  });
-  it("should be able to mock a post request", async () => {
-    const response = await zodios.post("/users", {
-      body: { name: "John Doe", email: "john.doe@test-post.com" },
+    it("should be able to mock errors of a request with params", async () => {
+      mocks.getResponse("/users/:id", {
+        status: 404,
+        data: {
+          message: "User not found",
+        },
+      });
+      let error;
+      try {
+        const response = await zodios.get("/users/:id", { params: { id: 2 } });
+      } catch (e) {
+        error = e;
+      }
+      expect(error).toBeDefined();
+      expect((error as any).response.status).toEqual(404);
     });
-    expect(response).toEqual({
-      id: 1,
-      name: "John Doe",
-      email: "john.doe@test-post.com",
+    it("should be able to mock a post request", async () => {
+      mocks.postResponse("/users", {
+        data: {
+          id: 1,
+          name: "John Doe",
+          email: "john.doe@test-post.com",
+        },
+      });
+
+      const response = await zodios.post("/users", {
+        body: { name: "John Doe", email: "john.doe@test-post.com" },
+      });
+      expect(response).toEqual({
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@test-post.com",
+      });
     });
-  });
-  it("should be able to mock a put request", async () => {
-    const response = await zodios.put("/users/:id", {
-      params: { id: 1 },
-      body: { id: 1, name: "John Doe", email: "john.doe@test-put.com" },
+    it("should be able to mock a put request", async () => {
+      mocks.putResponse("/users/:id", {
+        data: {
+          id: 1,
+          name: "John Doe",
+          email: "john.doe@test-put.com",
+        },
+      });
+
+      const response = await zodios.put("/users/:id", {
+        params: { id: 1 },
+        body: { id: 1, name: "John Doe", email: "john.doe@test-put.com" },
+      });
+      expect(response).toEqual({
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@test-put.com",
+      });
     });
-    expect(response).toEqual({
-      id: 1,
-      name: "John Doe",
-      email: "john.doe@test-put.com",
+    it("should be able to mock a patch request", async () => {
+      mocks.patchResponse("/users/:id", {
+        data: {
+          id: 1,
+          name: "John Doe",
+          email: "john.doe@test-patch.com",
+        },
+      });
+      const response = await zodios.patch("/users/:id", {
+        params: { id: 1 },
+        body: { id: 1, name: "John Doe", email: "john.doe@test-patch.com" },
+      });
+      expect(response).toEqual({
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@test-patch.com",
+      });
     });
-  });
-  it("should be able to mock a patch request", async () => {
-    const response = await zodios.patch("/users/:id", {
-      params: { id: 1 },
-      body: { id: 1, name: "John Doe", email: "john.doe@test-patch.com" },
-    });
-    expect(response).toEqual({
-      id: 1,
-      name: "John Doe",
-      email: "john.doe@test-patch.com",
-    });
-  });
-  it("should be able to mock a delete request", async () => {
-    const response = await zodios.delete("/users/:id", {
-      params: { id: 1 },
-    });
-    expect(response).toEqual({
-      id: 1,
-      name: "John Doe",
-      email: "john.doe@test.com",
+    it("should be able to mock a delete request", async () => {
+      mocks.deleteResponse("/users/:id", {
+        data: {
+          id: 1,
+          name: "John Doe",
+          email: "john.doe@test.com",
+        },
+      });
+
+      const response = await zodios.delete("/users/:id", {
+        params: { id: 1 },
+      });
+      expect(response).toEqual({
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@test.com",
+      });
     });
   });
 });
